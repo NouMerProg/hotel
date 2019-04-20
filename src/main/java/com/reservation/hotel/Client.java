717eee7c2ef8d522668d6a83e7c086cca1d2018a
MERSEL <noureddine.mersel@gmail.com>
2019-04-20 17:22:43 +0200

    [NME] - class Client

---
+++ src/main/java/com/reservation/hotel/Client.java
@@ -0,0 +1,16 @@
+package com.reservation.hotel;
+
+public class Client {
+	
+	private String nom;
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	
+}
